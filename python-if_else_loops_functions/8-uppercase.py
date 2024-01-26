dea3393015a4a5c05c6383ea4a6a163463ac927e
RSvntana <123roabel@gmail.com>
2024-01-26 02:34:47 -0400

    add all

---
+++ python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,6 @@
+def uppercase(str):
+    for str in str:
+        if ord(str) >= 97 and ord(str) <= 122:
+            str = chr(ord(str) - 32)
+        print("{}".format(str), end="")
+    print("")
