01ad9902e982518988dd40122b33906f2fc6c61b
RSvntana <123roabel@gmail.com>
2024-02-02 01:00:45 -0400

    fix code

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -3,21 +3,22 @@ def roman_to_int(roman_string):
     if type(roman_string) is not str or roman_string is None:
         return 0
 
-    roman_letter = [
-        ['M', 1000], ['D', 500], ['C', 100], ['L', 50],
-        ['X', 10], ['V', 5], ['I', 1]
-    ]
+    roman_values = {
+        'M': 1000, 'D': 500, 'C': 100,
+        'L': 50, 'X': 10, 'V': 5, 'I': 1
+    }
 
     number = 0
-    last = 0
-    for letter in roman_string:
-        for elm in roman_letter:
-            if letter == elm[0]:
-                if last == 0 or last >= elm[1]:
-                    number += elm[1]
-                elif last < elm[1]:
-                    number += elm[1] - (last * 2)
+    prev_value = 0
 
-                last = number
+    for letter in roman_string:
+        value = roman_values.get(letter, 0)
+
+        if value > prev_value:
+            number += value - 2 * prev_value
+        else:
+            number += value
+
+        prev_value = value
 
     return number
