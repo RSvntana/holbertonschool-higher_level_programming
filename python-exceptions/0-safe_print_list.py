b30a990569b6550433ed2ba3f3229876c0cce1c7
RSvntana <123roabel@gmail.com>
2024-02-02 01:25:44 -0400

    fix code

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
-def safe_print_lists(my_list=[], x=0):
-    ret = 0
-    for i in range(x):
-        try:
+def safe_print_list(my_list=[], x=0):
+    try:
+        for i in range(x):
             print("{}".format(my_list[i]), end="")
-            ret += 1
-        except IndexError:
-            break
-    print("")
-    return ret
+        print()
+        return x
+    except IndexError:
+        print()
+        return i
