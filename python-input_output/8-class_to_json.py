d434871aba793851916035b0caef276213b28d7b
RSvntana <123roabel@gmail.com>
2024-02-11 15:24:13 -0400

    task 8

---
+++ python-input_output/8-class_to_json.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""8. Class to JSON"""
+
+
+def class_to_json(obj):
+    """returns the dictionary description with simple data structure
+    (list, dictionary, string, integer and boolean) for JSON serialization
+    Args:
+        obj: object to be serialized
+    """
+    return obj.__dict__
