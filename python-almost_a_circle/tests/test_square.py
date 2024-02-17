335eccbd90a4b0101dec6cf79b2b82d61bf811c2
RSvntana <123roabel@gmail.com>
2024-02-17 16:37:09 -0400

    fix all

--- python-almost_a_circle/tests/test_square.py
+++ python-almost_a_circle/tests/test_square.py
@@ -5,17 +5,24 @@ from models.square import Square
 class TestSquare(unittest.TestCase):
 
     def test_str(self):
+        """Test string representation of Square."""
         s = Square(5, 3, 4, 1)
         self.assertEqual(str(s), "[Square] (1) 3/4 - 5")
 
     def test_size(self):
+        """Test size property and setter."""
         s = Square(10)
         self.assertEqual(s.size, 10)
+
         s.size = 20
-        self.assertEqual(s.size, s.width, s.height, 20)
+        self.assertEqual(s.size, 20)
+        self.assertEqual(s.width, 20)
+        self.assertEqual(s.height, 20)
 
     def test_update(self):
+        """Test update method of Square."""
         sq = Square(5, 3, 4, 1)
+
         sq.update(2)
         self.assertEqual(str(sq), "[Square] (2) 3/4 - 5")
 
@@ -29,6 +36,7 @@ class TestSquare(unittest.TestCase):
         self.assertEqual(str(sq), "[Square] (5) 5/2 - 10")
 
     def test_to_dictionary(self):
+        """Test to_dictionary method of Square."""
         s = Square(5, 3, 4, 1)
         d = s.to_dictionary()
         self.assertEqual(d, {'id': 1, 'size': 5, 'x': 3, 'y': 4})
