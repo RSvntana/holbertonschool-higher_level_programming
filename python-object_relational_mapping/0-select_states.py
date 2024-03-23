1d2577f5a41df03c0f84845d49c871521b9ef76b
RSvntana <123roabel@gmail.com>
2024-03-22 22:11:27 -0400

    task 0

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,39 +1,15 @@
 #!/usr/bin/python3
-"""
-This script lists all states from the database hbtn_0e_0_usa.
-It takes three command line arguments: MySQL username,
-password, and database name.
-"""
+"""print all states from a database"""
 
-# Import necessary modules
-import MySQLdb
-import sys
-"""This condition checks if the script is being run
-directly or imported as a module
-The code inside this condition will only run if the
-script is run directly"""
-if __name__ == "__main__":
-    """ Establish a connection to the MySQL database
-    sys.argv[1], sys.argv[2], and sys.argv[3] are command
-    line arguments for MySQL username, password, and database name"""
-    db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
-                         password=sys.argv[2], db=sys.argv[3])
 
-# Create a cursor object to execute SQL queries
+import MySQLdb as sql
+from sys import argv
+
+if __name__ == '__main__':
+    db = sql.connect(host="localhost",
+                     port=3306, user=argv[1], passwd=argv[2], db=argv[3])
     cur = db.cursor()
-
-# Execute a SQL query to select all states from the database,
-#  ordered by states.id in ascending order
-    cur.execute(
-        "SELECT states.id, states.name FROM states ORDER BY states.id ASC")
-
-# Fetch all rows from the result of the query
+    cur.execute("SELECT * FROM states")
     rows = cur.fetchall()
-
-# Loop through each row and print it
     for row in rows:
         print(row)
-
-# Close the cursor and the database connection
-    cur.close()
-    db.close()
