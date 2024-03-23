465c045bb1579fe0727b6bdb855a1c470cdfc852
RSvntana <123roabel@gmail.com>
2024-03-22 22:19:29 -0400

    task 3

--- python-object_relational_mapping/3-my_safe_filter_states.py
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,32 +1,19 @@
 #!/usr/bin/python3
-""" write a script that takes in arguments and displays all values
-in the states table of hbtn_0e_0_usa where name matches the argument.
-But this time, write one that is safe from MySQL injections!"""
+"""takes in an argument and displays
+all values in the states table of hbtn_0e_0_usa
+where name matches the argument"""
 
-import MySQLdb
-import sys
 
-if __name__ == "__main__":
-    """Connect to the dabatabase"""
-    db = MySQLdb.connect(host='localhost', port=3306, user=sys.argv[1],
-                         password=sys.argv[2], db=sys.argv[3])
-
-    """Create a cursor object"""
+import MySQLdb as sql
+from sys import argv
+if __name__ == '__main__':
+    db = sql.connect(host="localhost",
+                     port=3306, user=argv[1], passwd=argv[2], db=argv[3])
     cur = db.cursor()
-
-    """acreate the query and get the state
-    name from the command line
-    Like BINARY is used to make the search case sensitive"""
-    query = "SELECT * FROM states WHERE name LIKE BINARY \
-        %s ORDER BY states.id ASC"
-
-    """Execute the query"""
-    cur.execute(query, (sys.argv[4],))
-
-    """Fetch all rows"""
-    for row in cur.fetchall():
+    query = "SELECT * FROM states WHERE name LIKE BINARY %s"
+    cur.execute(query, (argv[4],))
+    rows = cur.fetchall()
+    for row in rows:
         print(row)
-
-    """Close cursor and db"""
     cur.close()
     db.close()
